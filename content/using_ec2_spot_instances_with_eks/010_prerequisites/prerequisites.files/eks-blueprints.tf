2cbe3c863c114b5e07494f4d6a1e97065cfaf8bd
Marc Severin <marcseverin@gmail.com>
2023-06-06 06:59:35 -0400

    Update eks-blueprints.tf terraform version of VPC module (#251)
    * Update eks-blueprints.tf terraform version of VPC module
    
    Updated terraform-aws-module-vpc to >4.0
    
    * Update eks-blueprints.tf
    
    Updated to 4.0.2 as recommended
    
    * Update eks-blueprints.tf
    
    Pinned version to current version

--- content/using_ec2_spot_instances_with_eks/010_prerequisites/prerequisites.files/eks-blueprints.tf
+++ content/using_ec2_spot_instances_with_eks/010_prerequisites/prerequisites.files/eks-blueprints.tf
@@ -159,7 +159,7 @@ module "eks_blueprints_kubernetes_addons" {
 #---------------------------------------------------------------
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 3.0"
+  version = "4.0.2"
 
   name = local.name
   cidr = local.vpc_cidr
