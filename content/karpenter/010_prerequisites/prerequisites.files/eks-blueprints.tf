7a398b182be9eb7f609af86fa91f351808f6da13
Robert Northard <northard@amazon.com>
2023-09-02 13:57:36 +0100

    simplify provisioning template

--- content/karpenter/010_prerequisites/prerequisites.files/eks-blueprints.tf
+++ content/karpenter/010_prerequisites/prerequisites.files/eks-blueprints.tf
@@ -67,9 +67,7 @@ locals {
   region = "--AWS_REGION--"
 
   cluster_version = "--EKS_VERSION--"
-
-  node_group_name = "managed-ondemand"
-
+  
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 
